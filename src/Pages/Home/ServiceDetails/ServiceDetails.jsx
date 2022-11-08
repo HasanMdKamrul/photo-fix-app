50f0444ae298a0504af3f47fb954c09b269cd6cd
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-11-08 03:18:22 +0100

    No reviews found toggling added

--- src/Pages/Home/ServiceDetails/ServiceDetails.jsx
+++ src/Pages/Home/ServiceDetails/ServiceDetails.jsx
@@ -15,6 +15,8 @@ const ServiceDetails = () => {
 
   console.log(data);
 
+  console.log(reviews);
+
   useEffect(() => {
     const loadReviews = async () => {
       try {
@@ -63,65 +65,71 @@ const ServiceDetails = () => {
         <h1 className="text-5xl dark:text-gray-200 text-center text-black font-extrabold mt-5 ">
           Reviews
         </h1>
-        <section class="bg-white dark:bg-gray-900">
-          <div class="container px-6 py-10 mx-auto">
-            <div class="mt-6 md:flex md:items-center md:justify-between">
-              <div>
-                <h1 class="text-3xl font-semibold text-gray-800 capitalize xl:text-5xl lg:text-4xl dark:text-white">
-                  What our clients are saying
-                </h1>
+        {reviews.length === 0 ? (
+          <h1 className="text-5xl text-center dark:text-gray-200">
+            No Reviews Found
+          </h1>
+        ) : (
+          <section class="bg-white dark:bg-gray-900">
+            <div class="container px-6 py-10 mx-auto">
+              <div class="mt-6 md:flex md:items-center md:justify-between">
+                <div>
+                  <h1 class="text-3xl font-semibold text-gray-800 capitalize xl:text-5xl lg:text-4xl dark:text-white">
+                    What our clients are saying
+                  </h1>
 
-                <div class="flex mx-auto mt-6">
-                  <span class="inline-block w-40 h-1 bg-blue-500 rounded-full"></span>
-                  <span class="inline-block w-3 h-1 mx-1 bg-blue-500 rounded-full"></span>
-                  <span class="inline-block w-1 h-1 bg-blue-500 rounded-full"></span>
+                  <div class="flex mx-auto mt-6">
+                    <span class="inline-block w-40 h-1 bg-blue-500 rounded-full"></span>
+                    <span class="inline-block w-3 h-1 mx-1 bg-blue-500 rounded-full"></span>
+                    <span class="inline-block w-1 h-1 bg-blue-500 rounded-full"></span>
+                  </div>
+                </div>
+
+                <div class="flex justify-between mt-8 md:mt-0">
+                  <button class="p-2 mx-3 text-gray-800 transition-colors duration-300 border rounded-full rtl:-scale-x-100 dark:text-gray-200 dark:hover:bg-gray-800 dark:border-gray-700 hover:bg-gray-100">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M15 19l-7-7 7-7"
+                      />
+                    </svg>
+                  </button>
+
+                  <button class="p-2 text-gray-800 transition-colors duration-300 border rounded-full rtl:-scale-x-100 dark:text-gray-200 dark:hover:bg-gray-800 dark:border-gray-700 hover:bg-gray-100">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M9 5l7 7-7 7"
+                      />
+                    </svg>
+                  </button>
                 </div>
               </div>
 
-              <div class="flex justify-between mt-8 md:mt-0">
-                <button class="p-2 mx-3 text-gray-800 transition-colors duration-300 border rounded-full rtl:-scale-x-100 dark:text-gray-200 dark:hover:bg-gray-800 dark:border-gray-700 hover:bg-gray-100">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="w-6 h-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M15 19l-7-7 7-7"
-                    />
-                  </svg>
-                </button>
-
-                <button class="p-2 text-gray-800 transition-colors duration-300 border rounded-full rtl:-scale-x-100 dark:text-gray-200 dark:hover:bg-gray-800 dark:border-gray-700 hover:bg-gray-100">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="w-6 h-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M9 5l7 7-7 7"
-                    />
-                  </svg>
-                </button>
-              </div>
+              <section class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 lg:grid-cols-2 xl:grid-cols-3">
+                {reviews.map((review) => (
+                  <ReviewCard key={review._id} review={review} />
+                ))}
+              </section>
             </div>
-
-            <section class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 lg:grid-cols-2 xl:grid-cols-3">
-              {reviews.map((review) => (
-                <ReviewCard key={review._id} review={review} />
-              ))}
-            </section>
-          </div>
-        </section>
+          </section>
+        )}
       </section>
       {user && user?.uid ? (
         <section>
