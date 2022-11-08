c3f3c16a9dcc7d891df2e40200329c3289a80372
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-11-08 06:31:52 +0100

    New service added to home page added by adding time and sort descending

--- src/Pages/AddService/AddService.jsx
+++ src/Pages/AddService/AddService.jsx
@@ -1,68 +1,131 @@
 import React from "react";
+import { toast } from "react-toastify";
 
 const AddService = () => {
+  const submitHandle = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+
+    const service = {
+      title: form.title.value,
+      price: form.price.value,
+      description: form.description.value,
+      category: form.category.value,
+      image: form.image.value,
+    };
+
+    const addService = async () => {
+      try {
+        const response = await fetch(`http://localhost:15000/createservice`, {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(service),
+        });
+        const data = await response.json();
+
+        if (data.success) {
+          toast.success(data.message);
+        }
+      } catch (error) {
+        toast.error(error.message);
+      }
+    };
+
+    addService();
+  };
+
+  const inputFields = (
+    <>
+      <div>
+        <label className="text-gray-700 dark:text-gray-200" htmlFor="username">
+          Title
+        </label>
+        <input
+          name="title"
+          id="title"
+          type="text"
+          className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+        />
+      </div>
+
+      <div>
+        <label
+          className="text-gray-700 dark:text-gray-200"
+          htmlFor="emailAddress"
+        >
+          Price
+        </label>
+        <input
+          name="price"
+          id="price"
+          type="text"
+          className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+        />
+      </div>
+
+      <div>
+        <label className="text-gray-700 dark:text-gray-200" htmlFor="password">
+          Description
+        </label>
+        <textarea
+          name="description"
+          id="description"
+          type="text"
+          className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+        />
+      </div>
+
+      <div>
+        <label
+          className="text-gray-700 dark:text-gray-200"
+          htmlFor="passwordConfirmation"
+        >
+          Category
+        </label>
+        <input
+          name="category"
+          id="category"
+          type="text"
+          className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+        />
+      </div>
+      <div>
+        <label
+          className="text-gray-700 dark:text-gray-200"
+          htmlFor="passwordConfirmation"
+        >
+          Image
+        </label>
+        <input
+          name="image"
+          id="image"
+          type="text"
+          className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+        />
+      </div>
+    </>
+  );
+
   return (
     <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800 my-12 ">
       <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
-        Account settings
+        Add Service
       </h2>
 
-      <form>
+      <form onSubmit={submitHandle}>
         <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-          <div>
-            <label className="text-gray-700 dark:text-gray-200" for="username">
-              Username
-            </label>
-            <input
-              id="username"
-              type="text"
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-            />
-          </div>
-
-          <div>
-            <label
-              className="text-gray-700 dark:text-gray-200"
-              for="emailAddress"
-            >
-              Email Address
-            </label>
-            <input
-              id="emailAddress"
-              type="email"
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-            />
-          </div>
-
-          <div>
-            <label className="text-gray-700 dark:text-gray-200" for="password">
-              Password
-            </label>
-            <input
-              id="password"
-              type="password"
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-            />
-          </div>
-
-          <div>
-            <label
-              className="text-gray-700 dark:text-gray-200"
-              for="passwordConfirmation"
-            >
-              Password Confirmation
-            </label>
-            <input
-              id="passwordConfirmation"
-              type="password"
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-            />
-          </div>
+          {inputFields}
         </div>
 
         <div className="flex justify-end mt-6">
-          <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
-            Save
+          <button
+            type="submit"
+            className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+          >
+            Add Service
           </button>
         </div>
       </form>
